489dcbaa499e1a009d4517d205b8be857937c7e0
dartlenin <gaidai92@yandex.ru>
2016-03-21 12:23:13 +0300

    added training set

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ def main():
     neighbours_amount = 6
     solvent_radius = 1.5
     data_path = sys.argv[1]
-    output_file_path = sys.argv[2]
-    modification_full_name = sys.argv[3]
+    output_file_path = r"E:\Dropbox\masters_work\r_project\met6_1.5.csv"
+    modification_full_name = sys.argv[2]
     with open(output_file_path, "w") as output:
         feature_collector = FeatureCollector(neighbours_amount=neighbours_amount,
                                              protein_structure_args={"solvent_radius": solvent_radius})
